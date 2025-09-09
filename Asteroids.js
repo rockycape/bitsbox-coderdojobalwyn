7407fda873d7fe54453b946241dcbfbb1c071c44
rockycape <19771537+rockycape@users.noreply.github.com>
2025-09-09 15:25:58 +1000

    update

--- Asteroids.js
+++ Asteroids.js
@@ -1,32 +1,41 @@
-fill('stars2')
+
 points = 0
-rocket = stamp('rocket', 180)   // --- size of rocket is 180
-rocket.scale = 0.5
+size = 225
+speed = 10
+rocket = stamp('rocket', size)
 rocket.vx = 0
 rocket.vy = 0
 
 score = text(points, 20, 20, 'white')
-message = text('Ready?', 280, 500, 40, 'white')
+message = text('Ready?', 330, 650, 40, 'black')
 
 // --- Controls ---
-function tap() {
+function shoot() {
   sound('laser')
-  shot = stamp('laser', rocket.x, rocket.y)
-  shot.angle = rocket.angle
-  dx = 10 * Math.sin(rocket.angle * Math.PI/180)
-  dy = -10 * Math.cos(rocket.angle * Math.PI/180)
-  shot.vx = dx
-  shot.vy = dy
+  // Use antenna4 as bullet, fire from rocket's nose
+  var offset = 50
+  var rad = rocket.angle * Math.PI / 180
+  var sx = rocket.x + offset * Math.sin(rad)
+  var sy = rocket.y - offset * Math.cos(rad)
+  shot = stamp('antenna4', sx, sy, 40)
+  // Move bullet in direction rocket is facing
+  var moveDir = rocket.angle
+  shot.move(moveDir, 1500, 2000)
 }
 
-function drag(x, y) {
-  // Point ship toward drag location
-  rocket.aim(x, y)
+// Automatic shooting every half second
+function autoShoot() {
+  shoot()
+  delay(autoShoot, 2000)
+}
+autoShoot()
 
-  // Apply thrust in that direction
-  thrust = 0.3
-  rocket.vx += thrust * Math.sin(rocket.angle * Math.PI/180)
-  rocket.vy += -thrust * Math.cos(rocket.angle * Math.PI/180)
+function drag(x, y) {
+  // Use astronaut movement logic
+  spot = x - 384
+  thrust = (750 - y) / 100 * speed
+  rocket.move('up', thrust)
+  rocket.rotate(spot / 2)
 }
 
 // --- Asteroids ---
@@ -94,11 +103,7 @@ function animate() {
   })
 
   // Collision: rocket vs asteroid
-  if (rocket.hits('asteroid')) {
-    rocket.explode()
-    message.change('Game Over')
-    loop = null
-  }
+  // Rocket is indestructible: do nothing on collision
 }
 
 function startGame() {
